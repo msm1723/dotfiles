4799ef6f11e6cd7ed48cfc4e0f868279fda2b17b
msm1723 <msm1723@gmailcom>
2024-12-14 16:56:47 +0400

    nvim complete

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,3 @@
--- Expand the path to the dotfiles directory
--- local home_dir = vim.fn.expand('~')
--- local dotfiles_path = home_dir .. '/gitlocal/dotfiles/nvim/'
-
--- Add the dotfiles directory to the package path to allow require('config.lazy')
--- package.path = package.path .. ';' .. dotfiles_path .. '?.lua;' .. dotfiles_path .. '?/init.lua;' .. dotfiles_path .. '?/?.lua'
-
--- Require main configuration files from ~/dotfiles/nvim/config/
 require("custom.lazy")
 require("custom.settings")
+require("custom.maps")
--- nvim/lua/custom/plugins/colorschema.lua
+++ nvim/lua/custom/plugins/colorschema.lua
@@ -1,15 +1,15 @@
 return {
-  {
-    'shaunsingh/nord.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- Disable italic styles
-      vim.g.nord_italic = false
-      vim.g.nord_italic_comments = false
+	{
+		"shaunsingh/nord.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- Disable italic styles
+			vim.g.nord_italic = false
+			vim.g.nord_italic_comments = false
 
-      -- Now load Nord
-      vim.cmd('colorscheme nord')
-    end,
-  },
+			-- Now load Nord
+			vim.cmd("colorscheme nord")
+		end,
+	},
 }
