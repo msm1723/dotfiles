291b5a0f6ab6e48eb7f22758df3fc378722bd635
msm1723 <msm1723@gmail.com>
2025-03-17 03:15:51 +0300

    nvim0.1

---
+++ nvim/lua/settings/keymaps.lua
@@ -0,0 +1,84 @@
+-- Set leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = '\\'
+
+local function map(mode, lhs, rhs)
+	vim.keymap.set(mode, lhs, rhs, { noremap = true, silent = false })
+end
+
+-- Disable the spacebar key's default behavior in Normal and Visual modes
+map({ 'n', 'v' }, '<Space>', '<Nop>')
+
+-- Save
+map('n', '<leader>w', '<CMD>update<CR>')
+
+-- Quit
+map('n', '<leader>q', '<CMD>q<CR>')
+
+-- Exit insert mode
+map('i', 'jk', '<ESC>')
+
+-- NeoTree
+map('n', '<leader>e', '<CMD>Neotree toggle<CR>')
+map('n', '<leader>r', '<CMD>Neotree focus<CR>')
+
+-- Delete single character without copying into register
+map('n', 'x', '"_x')
+
+-- Vertical scroll half screen down/up and center
+map('n', '<C-d>', '<C-d>zz')
+map('n', '<C-u>', '<C-u>zz')
+
+-- Find next/previous and center
+map('n', 'n', 'nzzzv')
+map('n', 'N', 'Nzzzv')
+
+-- Clear highlighting
+map('n', '<Esc>', ':nohlsearch<CR>')
+
+-- Window managemen
+map('n', '<leader>v', '<C-w>v', opts) -- split window vertically
+map('n', '<leader>h', '<C-w>s', opts) -- split window horizontally
+map('n', '<leader>se', '<C-w>=', opts) -- make split windows equal width & height
+map('n', '<leader>xs', '<CMD>close<CR>', opts) -- close current split window-- Window Navigation
+
+-- Navigate between splits
+map('n', '<C-h>', '<C-w>h')
+map('n', '<C-l>', '<C-w>l')
+map('n', '<C-k>', '<C-w>k')
+map('n', '<C-j>', '<C-w>j')
+
+-- Resize Windows
+map('n', '<C-Left>', '<C-w><')
+map('n', '<C-Right>', '<C-w>>')
+map('n', '<C-Up>', '<C-w>+')
+map('n', '<C-Down>', '<C-w>-')
+
+-- Buffers
+map('n', '<Tab>', '<CMD>bnext<CR>')
+map('n', '<S-Tab>', '<CMD>bprevious<CR>')
+map('n', '<leader>x', '<CMD>bdelete!<CR>') -- close buffer
+map('n', '<leader>b', '<CMD>enew<CR>') -- new buffe
+
+-- Tabs
+map('n', '<leader>to', ':tabnew<CR>') -- open new tab
+map('n', '<leader>tx', ':tabclose<CR>') -- close current tab
+map('n', '<leader>tn', ':tabn<CR>') --  go to next tab
+map('n', '<leader>tp', ':tabp<CR>') --  go to previous tab
+
+-- Toggle line wrapping
+map('n', '<leader>lw', '<cmd>set wrap!<CR>')
+
+-- Stay in indent mode
+map('v', '<', '<gv')
+map('v', '>', '>gv')
+
+-- Keep last yanked when pasting
+map('v', 'p', '"_dP')
+
+-- Diagnostic keymaps
+map('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+map('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+map('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
