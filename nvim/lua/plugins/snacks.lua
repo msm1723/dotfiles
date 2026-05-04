fb9d3a60b9dbf3f7c811dc2f6e3ce04a79d54663
msm1723 <msm1723@gmail.com>
2026-05-05 01:32:06 +0300

    snacks

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    opts = {
+      bigfile = {
+        enabled = true,
+      },
+
+      dashboard = {
+        enabled = true,
+      },
+
+      input = {
+        enabled = true,
+      },
+    },
+  },
+}
